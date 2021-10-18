c3fd3c4d0a234e0267807dce3c7a5c18aeb1f17a
Akshay Agarawal <akshay@Akshays-MacBook-Air.local>
2021-10-18 16:47:13 +0200

    new commit

---
+++ src/main/java/com/checkout/CheckoutChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.checkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutChallengeApplication.class, args);
+	}
+
+}
